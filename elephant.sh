240ae633c9ece735f1058ecdd7e9b6180c73dcee
deep-nav <navdeep.dhuti@cameronandwilding.com>
2017-04-06 11:48:11 +0100

    Automating build

---
+++ elephant.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+build() {
+	echo "Building APK"
+	echo "============"
+	cordova build --release android
+	echo "============"
+	echo "Signing APK"
+	echo "============"
+	jarsigner -verbose -sigalg SHA1withRSA\
+	 -digestalg SHA1 -keystore\
+	  Archive/keys/my-release-key.keystore\
+	  platforms/android/build/outputs/apk/android-release-unsigned.apk alias_name
+	echo "============"
+	echo "Optimising APK"
+	echo "============"
+	zipalign -v 4\
+	platforms/android/build/outputs/apk/android-release-unsigned.apk\
+	Archive/elephant'_'`date +%s`.apk
+}
+
+$1
